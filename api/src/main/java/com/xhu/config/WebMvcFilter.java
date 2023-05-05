ea7f433ef5ab62347a2fd1be8db69827dd2fcc9b
leeqi10 <447789814@qq.com>
2023-05-05 19:36:06 +0800

    修正前端页面

--- api/src/main/java/com/xhu/config/WebMvcFilter.java
+++ api/src/main/java/com/xhu/config/WebMvcFilter.java
@@ -3,6 +3,8 @@ package com.xhu.config;
 import com.xhu.filter.CustomerLogin;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 @Configuration
@@ -10,6 +12,25 @@ public class WebMvcFilter extends WebMvcConfigurationSupport {
     @Override
     protected void addInterceptors(InterceptorRegistry registry) {
 
-        registry.addInterceptor(new CustomerLogin());
+        registry.addInterceptor(new CustomerLogin()).addPathPatterns("/**")
+                .excludePathPatterns("/**/*.css")
+                .excludePathPatterns("/**/*.js")
+                .excludePathPatterns("/**/*.png")
+                .excludePathPatterns("/**/*.jpg")
+                .excludePathPatterns("/swagger/*")
+                .excludePathPatterns("/doc.html/**")
+                .excludePathPatterns("/**/*.jpeg");
+        super.addInterceptors(registry);
+    }
+
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations("classpath:/static");
+        super.addResourceHandlers(registry);
+    }
+
+    @Override
+    protected void addViewControllers(ViewControllerRegistry registry) {
+        registry.addRedirectViewController("/","/static/index.html");
+        super.addViewControllers(registry);
     }
 }
